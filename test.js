57aeead02f15801126ef7baf100ec0eddf4c71e6
mich <843298603@qq.com>
2019-07-07 01:13:17 +0800

    http.createServer(0.0.2)

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ let http = require('http')
 
 let server = http.createServer(function (req,res){
     console.log(req.url)
-    
+    res.statusCode = 201
+    res.write('hello')
+    res.end()
 })
 server.listen(9999)
 console.log('9999')
