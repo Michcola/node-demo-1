86372192da284e4c44354319ff36ec626fbec4da
mich <843298603@qq.com>
2019-07-07 12:19:18 +0800

    koa server test

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const send = require('koa-send');
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+app.use(async (ctx) => {
+    if('/' == ctx.path) return ctx.body = 'Try GET /package.json';
+    await send(ctx, ctx.path);
 });
 
 app.listen(9998);
+console.log('listening on port 9998');
